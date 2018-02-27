053249c807aa70cb41fa2c7339c2284430701d00
js-jslog <josephsinfield@yahoo.com>
2018-02-27 00:48:18 +0000

    including home nav aliases to .bash_aliases and modifying setup.sh to deploy it

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 rm ~/.tmux.conf || true
 rm ~/.vimrc || true
-sudo rm -r ~/.vim || true
-sudo rm -r ~/.config/nvim || true
+rm ~/.bash_aliases || true
 mkdir -p ~/.vim/bundle
+sudo rm -r ~/.vim || true
+cp ./.bash_aliases ~/.bash_aliases
 cp .tmux.conf ~/.
 cp .vimrc ~/.
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
