71f04962500bad3320c4c65072fdf6602cfb06c6
js-jslog <jhs4jbs@hotmail.co.uk>
2018-03-19 17:58:35 +0000

    including .bash_functions_rsl in the setup script

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 rm ~/.tmux.conf || true
 rm ~/.vimrc || true
 rm ~/.bash_aliases || true
+rm ~/.bash_functions_rsl || true
 mkdir -p ~/.vim/bundle
 sudo rm -r ~/.vim || true
 cp ./.bash_aliases ~/.bash_aliases
+cp ./.bash_functions_rsl ~/.bash_functions_rsl
 cp .tmux.conf ~/.
 cp .vimrc ~/.
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
