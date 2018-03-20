657a779e8a3c9ac947dcf45ba2e1f87bfcac3198
js-jslog <jhs4jbs@hotmail.co.uk>
2018-03-20 08:36:10 +0000

    including .bashrc and created non-clobbering method of keeping the original file, just incase it's run on a machine which has customised it's bashrc

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ rm ~/.bash_aliases || true
 rm ~/.bash_functions_rsl || true
 mkdir -p ~/.vim/bundle
 sudo rm -r ~/.vim || true
+cp -n ~/.bashrc ~/.bashrc.original
+cp -rf ./.bashrc ~/.bashrc
 cp ./.bash_aliases ~/.bash_aliases
 cp ./.bash_functions_rsl ~/.bash_functions_rsl
 cp .tmux.conf ~/.
