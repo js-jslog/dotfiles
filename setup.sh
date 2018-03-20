581bedf7f24768de2f3de26e8fc462221a5e0370
js-jslog <jhs4jbs@hotmail.co.uk>
2018-03-20 08:55:47 +0000

    replacing rm's with clobbering cp's

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-rm ~/.tmux.conf || true
-rm ~/.vimrc || true
-rm ~/.bash_aliases || true
-rm ~/.bash_functions_rsl || true
 mkdir -p ~/.vim/bundle
 sudo rm -r ~/.vim || true
 cp -n ~/.bashrc ~/.bashrc.original
 cp -rf ./.bashrc ~/.bashrc
-cp ./.bash_aliases ~/.bash_aliases
-cp ./.bash_functions_rsl ~/.bash_functions_rsl
-cp .tmux.conf ~/.
-cp .vimrc ~/.
+cp -rf ./.bash_aliases ~/.bash_aliases
+cp -rf ./.bash_functions_rsl ~/.bash_functions_rsl
+cp -rf .tmux.conf ~/.
+cp -rf .vimrc ~/.
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
