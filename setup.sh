7ccc818761d3b3571de618f597f317f503188c18
js-jslog <josephsinfield@yahoo.com>
2018-03-21 22:35:06 +0000

    adding ssh config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo rm -r ~/.vim || true
+mkidr -p ~/.ssh
+cp -rf ./.ssh/config ~/.ssh/config
 cp -n ~/.bashrc ~/.bashrc.original
 cp -rf ./.bashrc ~/.bashrc
 cp -rf ./.bash_aliases ~/.bash_aliases
