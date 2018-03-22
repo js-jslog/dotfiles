5d1cd53923a802f3897249dff23b4f439d73e19b
js-jslog <josephsinfield@yahoo.com>
2018-03-22 21:40:46 +0000

    improving setup so that it deletes files & symlinks before copying it's files across. soon this won't be necessary

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo rm -r ~/.vim || true
 mkidr -p ~/.ssh
-cp -rf ./.ssh/config ~/.ssh/config
+cp --remove-destination ./.ssh/config ~/.ssh/config
 cp -n ~/.bashrc ~/.bashrc.original
-cp -rf ./.bashrc ~/.bashrc
-cp -rf ./.bash_aliases ~/.bash_aliases
-cp -rf ./.bash_functions_rsl ~/.bash_functions_rsl
-cp -rf .tmux.conf ~/.
-cp -rf .vimrc ~/.
+cp --remove-destination ./.bashrc ~/.bashrc
+cp --remove-destination ./.bash_aliases ~/.bash_aliases
+cp --remove-destination ./profile.d/bashfunctions.sh /etc/profile.d/bashfunctions.sh
+cp --remove-destination .tmux.conf ~/.
+cp --remove-destination .vimrc ~/.
 git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
