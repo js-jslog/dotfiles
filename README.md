cd7bd6a8eae9ecfcf1c2d8ccb8262d4f7ccad707
js-jslog <josephsinfield@yahoo.com>
2018-03-23 00:02:25 +0000

    adding the bash.d folder and modifying the setup script to deal with it and use symlinks instead of copied files

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 configuration files in home directory
 
 # usage
+there are two options for running this dotfile setup
+
+## standalone
 simply run sh setup.sh from this directory
 WARNING: this will delete your existing .vimrc & .tmux.conf files as well as your existing .vim folder
+
+## multiplexed
+to make this dotfiles configuration part of a multiplexed set of files follow the instructions at https://github.com/js-jslog/dotfiles-root
--- setup.sh
+++ setup.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
-sudo rm -r ~/.vim || true
-mkidr -p ~/.ssh
-cp --remove-destination ./.ssh/config ~/.ssh/config
-cp -n ~/.bashrc ~/.bashrc.original
-cp --remove-destination ./.bashrc ~/.bashrc
-cp --remove-destination ./.bash_aliases ~/.bash_aliases
-cp --remove-destination ./profile.d/bashfunctions.sh /etc/profile.d/bashfunctions.sh
-cp --remove-destination .tmux.conf ~/.
-cp --remove-destination .vimrc ~/.
-git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+
+# if any original files exist then we will just move them rather than 
+# delete them
+# if the file is a symlink then it will have in all likelyhood been 
+# provisioned by a version controlled dotfile manager so can just
+# be overwritten by stage 2 below
+if [[ ! -L ~/.bashrc ]]; then
+  mv ~/.bashrc ~/.bashrc.original 2>/dev/null
+fi
+if [[ ! -L ~/.bash_aliases ]]; then
+  mv ~/.bash_aliases ~/.bash_aliases.original 2>/dev/null
+fi
+if [[ ! -L ~/.vimrc ]]; then
+  mv ~/.vimrc ~/.vimrc.original 2>/dev/null
+fi
+if [[ ! -L ~/.tmux.conf ]]; then
+  mv ~/.tmux.conf ~/.tmux.conf.original 2>/dev/null
+fi
+
+# overwrite existing symbolic links if they exist
+ln -sf ~/dotfiles-personal/.bashrc ~/.bashrc
+ln -sf ~/dotfiles-persoanl/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles-persoanl/.vimrc ~/.vimrc
+ln -sf ~/dotfiles-personal/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles-personal/bash.d ~/bash.d
+
+# filling the profile.d folder with scripts to be run at login shell initiation
+# profile files should contain exported environment variables and functions for login shells
+if [ -d ~/dotfiles-personal/profile.d/ ]; then
+  for profilepath in $(find ~/dotfiles-personal/profile.d/ \( -name '*.sh' \))
+  do
+    profilename=$(basename "${profilepath}")
+    sudo ln -sf $profilepath /etc/profile.d/personal-$profilename
+  done
+fi
