4b42a3091c294f58a968c96a4e555a5bf5a0f955
js-jslog <jhs4jbs@hotmail.co.uk>
2018-05-06 12:09:13 +0100

    adding ctags to my provisioning script

--- provision/run.sh
+++ provision/run.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# prerequisite for adding repos
 sudo apt-get install software-properties-common
+
+# install vim8
 sudo apt-get update
 sudo -E add-apt-repository ppa:jonathonf/vim
 sudo apt update
 sudo apt install vim
+
+# install ctags
+sudo apt-get install ctags
